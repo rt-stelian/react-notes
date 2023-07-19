041b8b7bfbe9c77f8a6150fb83f71b7fd728e553
Stelian <121059803+rt-stelian@users.noreply.github.com>
2023-07-19 10:46:48 +0300

    Move build folder to build-branch

--- src/components/UI/Button.jsx
+++
@@ -1,9 +0,0 @@
-const Button = ({ children, type, disabled, className }) => {
-  return (
-    <button className={className} disabled={disabled} type={type}>
-      {children}
-    </button>
-  )
-}
-
-export default Button
